63e770bc670a90edb8b66a72941a2a5f7902fca4
Alexey Yakovenko <wakeroid@gmail.com>
2021-02-16 18:07:39 +0100

    libsndfile warning fixes

--- libsndfile-1.0.28/src/sfconfig.h
+++ libsndfile-1.0.28/src/sfconfig.h
@@ -113,7 +113,11 @@
 #define HAVE_X86INTRIN_H 0
 #endif
 
-#define CPU_IS_X86		(defined __i486__ || defined __i586__ || defined __i686__ || defined __x86_64__)
-#define CPU_IS_X86_64	(defined __x86_64__)
+#if (defined __i486__ || defined __i586__ || defined __i686__ || defined __x86_64__)
+#define CPU_IS_X86 1
+#endif
 
+#if (defined __x86_64__)
+#define CPU_IS_X86_64 1
+#endif
 #endif
