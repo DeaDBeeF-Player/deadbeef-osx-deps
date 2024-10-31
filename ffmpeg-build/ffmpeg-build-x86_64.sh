580f100f881e41566391616f3e74ccb466275274
Oleksiy Yakovenko <wakeroid@gmail.com>
2024-10-31 19:50:28 +0100

    osx: rebuild ffmpeg 4.4 with eac3 support

--- ffmpeg-build/ffmpeg-build-x86_64.sh
+++ ffmpeg-build/ffmpeg-build-x86_64.sh
@@ -1,24 +1,27 @@
 # This is the commands used to build the ffmpeg libs provided here
 set -ex
-./configure --extra-cflags="-fPIC -isysroot /Applications/Xcode_13_1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -mmacosx-version-min=10.13" --extra-ldflags="-isysroot /Applications/Xcode_13_1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -mmacosx-version-min=10.13"\
+./configure --extra-cflags="-fPIC -isysroot /Applications/Xcode_15_2_0.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -mmacosx-version-min=10.13" --extra-ldflags="-isysroot /Applications/Xcode_15_2_0.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -mmacosx-version-min=10.13"\
     --target-os=darwin\
     --enable-shared --disable-static\
     --prefix="$PWD/build/x86_64"\
     --enable-pic --enable-gpl --disable-doc --disable-ffplay\
     --disable-ffprobe --disable-avdevice --disable-ffmpeg\
     --disable-videotoolbox\
+    --disable-demuxers --disable-decoders --disable-parsers\
     --disable-postproc --disable-swresample --disable-avfilter\
     --disable-swscale --enable-network --disable-swscale-alpha --disable-vdpau\
     --disable-dxva2 --enable-hwaccels\
     --disable-encoders --disable-muxers --disable-indevs --disable-outdevs\
-    --disable-devices --disable-filters --disable-parsers --enable-parser=ac3\
-    --enable-demuxer=ac3 --disable-bsfs --disable-bzlib --disable-protocols\
-    --disable-decoders --disable-decoder=libopus --disable-libopus\
+    --disable-devices --disable-filters\
+    --disable-bsfs --disable-bzlib --disable-protocols\
+    --disable-decoder=libopus --disable-libopus\
+    --enable-parser=ac3 --enable-demuxer=ac3 --enable-decoder=ac3\
+    --enable-demuxer=eac3 --enable-decoder=eac3\
     --enable-decoder=wmapro --enable-decoder=wmav1 --enable-decoder=wmav2\
-    --enable-decoder=wmavoice --enable-decoder=ac3\
+    --enable-decoder=wmavoice\
     --disable-decoder=amrnb\
-    --disable-demuxers --enable-demuxer=asf\
-    --enable-demuxer=mov --enable-demuxer=oma --enable-demuxer=ac3\
+     --enable-demuxer=asf\
+    --enable-demuxer=mov --enable-demuxer=oma\
     --disable-demuxer=amr --enable-demuxer=ogg\
     --enable-demuxer=tak --enable-decoder=tak\
     --enable-decoder=dsd_lsbf --enable-decoder=dsd_lsbf_planar\
@@ -28,7 +31,8 @@ set -ex
     --disable-libopencore-amrnb --disable-libopencore-amrwb\
     --disable-version3 --disable-mmx\
     --disable-x86asm --disable-inline-asm --disable-asm\
-    --enable-protocol=file --enable-protocol=http
+    --enable-protocol=file --enable-protocol=http\
+    --enable-audiotoolbox
 
 make -j8
 
